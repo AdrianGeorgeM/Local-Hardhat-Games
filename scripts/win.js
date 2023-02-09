6dfc038b682db6270e546af6100f9de9a76ccd1c
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-09 19:07:46 +0000

    Game1 contract deployed and pass the test
    Pass Reference in ReadMe

--- scripts/win.js
+++ scripts/win.js
@@ -1,23 +1,23 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = '0x3Aa5ebB10DC797CAC828524e59A333d0A371443c';
+const contractName = 'Game1';
 
 async function main() {
-    // attach to the game
-    const game = await hre.ethers.getContractAt(contractName, gameAddr);
+	// attach to the game
+	const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
-    // do whatever you need to do to win the game here:
-    const tx = await game.win();
+	// do whatever you need to do to win the game here:
+	const tx = await game.win();
 
-    // did you win? Check the transaction receipt!
-    // if you did, it will be in both the logs and events array
-    const receipt = await tx.wait();
-    console.log(receipt);
+	// did you win? Check the transaction receipt!
+	// if you did, it will be in both the logs and events array
+	const receipt = await tx.wait();
+	console.log(receipt);
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
